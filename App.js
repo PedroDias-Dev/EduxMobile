62c66bec3825e9ec5b6ba9dc31f1605f1f467fcb
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-11-27 17:10:26 -0300

    Postagens

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 // Navigations
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,17 +15,18 @@ const Stack  = createStackNavigator();
 // Paginas
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Home  from './pages/Home';
+import Postagens from './pages/Postagens';
+import Home  from './pages/Home (Ranking)';
 // import Eventos  from './pages/Eventos';
 import Turmas from './pages/Turmas';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Autenticado = () => {
   return(
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator initialRouteName="Postagens">
+        <Drawer.Screen name="Postagens" component={Postagens} />
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Logout" component={Logout} />
-        {/* <Drawer.Screen name="Eventos" component={Eventos} /> */}
     </Drawer.Navigator>
   )
 }
@@ -40,18 +43,32 @@ const Logout = ( {navigation} ) => {
   )
 }
 
+// const Tab = createBottomTabNavigator();
+
+// function BottomTab() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Postagens" component={Postagens} />
+//       <Tab.Screen name="Turmas" component={Turmas} />
+//     </Tab.Navigator>
+//   );
+// }
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown : false }}>
-      <Stack.Screen name="Turmas" component={Turmas} />
+        <Stack.Screen name="Postagens" component={Postagens} />
+        <Stack.Screen name="Turmas" component={Turmas} />
         <Stack.Screen name="Login" component={Login} />
         <Drawer.Screen name="Register" component={Register} />
         <Stack.Screen name="Autenticado" component={Autenticado} />
       </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
